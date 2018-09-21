7b61ffeb3ebcf4f99994bb97742444261f766ecf
Unknown <dominik.trusinski@gmail.com>
2018-09-21 23:42:56 +0200

    Delete unnecessary files. Add MarkerInfoWindowService tests

--- src/app/services/marker-info-window/marker-info-window.service.spec.ts
+++ src/app/services/marker-info-window/marker-info-window.service.spec.ts
@@ -1,15 +1,33 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { MarkerInfoWindowService } from './marker-info-window.service';
+import { SportObject } from '../../models/sport-object';
 
 describe('MarkerInfoWindowService', () => {
+  let service: MarkerInfoWindowService = null;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [MarkerInfoWindowService]
     });
+
+    service = TestBed.get(MarkerInfoWindowService);
   });
 
-  it('should be created', inject([MarkerInfoWindowService], (service: MarkerInfoWindowService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  it('should have proper content', () => {
+    const sportObject: SportObject = new SportObject(1, 'Pływalnia Polonez', { latitude: 52.299046, longitude: 21.033690 }),
+          actualContent = service.generateInfoWindowContent(sportObject),
+          expectedContent = `<div>` +
+                            `<p class="lead">${sportObject.name}</p>` +
+                            `<p>Dostępne dyscypliny sportowe:</p>` +
+                            `<ul><li>Piłka nożna</li><li>Piłka halowa</li></ul>` +
+                            `<a href="/sport_objects/${sportObject.id}">Przejdź do strony obiektu</a>` +
+                            `</div>`;
+
+    expect(actualContent).toEqual(expectedContent);
+  });
 });
