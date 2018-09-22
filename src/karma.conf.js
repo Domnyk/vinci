e825aeb32bb767e8177ee9208828b3ed72e9a1ca
Dominik Trusinski <dominik.trusinski@gmail.com>
2018-09-22 20:36:16 +0200

    Update to CI config. Use custom image

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -31,7 +31,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['InsecureChromeHeadless'],
+    customLaunchers: {
+      InsecureChromeHeadless: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
