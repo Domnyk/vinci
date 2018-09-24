f8d8a1267a4f3a66cde60e705d541b581e38b5eb
Unknown <dominik.trusinski@gmail.com>
2018-09-24 13:29:16 +0200

    Add sport object component

---
+++ src/app/new-sport-object/new-sport-object.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewSportObjectComponent } from './new-sport-object.component';
+
+describe('NewSportObjectComponent', () => {
+  let component: NewSportObjectComponent;
+  let fixture: ComponentFixture<NewSportObjectComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NewSportObjectComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewSportObjectComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/new-sport-object/new-sport-object.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-sport-object',
+  templateUrl: './new-sport-object.component.html',
+  styleUrls: ['./new-sport-object.component.css']
+})
+export class NewSportObjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
