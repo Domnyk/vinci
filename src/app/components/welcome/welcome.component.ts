416bc85b80c6e38d3cad3b0949f7ab218da32cce
Dominik Trusinski <dominik.trusinski@gmail.com>
2018-10-02 09:14:03 +0200

    Delete cookie code from welcome component

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-welcome',
@@ -7,18 +6,9 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-  private readonly existingUserCookieName = 'existingUser';
 
-  constructor(
-    private cookieService: CookieService
-  ) { }
+  constructor() { }
 
-  // TODO: Add secuirty options for cookie
-  ngOnInit() {
-    const isNewUser = !this.cookieService.check(this.existingUserCookieName);
-    if (isNewUser) {
-      this.cookieService.set(this.existingUserCookieName, 'Vinci@2018');
-    }
-  }
+  ngOnInit() { }
 
 }
