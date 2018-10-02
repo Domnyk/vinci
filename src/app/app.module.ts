3d2977c93f498c37ec695256245052fdda9807fa
Dominik Trusinski <dominik.trusinski@gmail.com>
2018-10-02 09:09:36 +0200

    Delete unnecessary import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,6 @@ import { NewSportObjectComponent } from './components/new-sport-object/new-sport
 import { ModalComponent } from './components/components.common/modal/modal.component';
 import { SportComplexDashboardComponent } from './components/components.admin-dashboard/sport-complex-dashboard/sport-complex-dashboard.component';
 import { SportObjectState } from './state/sport-object.state';
-import { CookieService } from 'ngx-cookie-service';
 
 
 @NgModule({
