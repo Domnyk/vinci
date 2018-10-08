e5b157733488e199e5682bf12275dbe8d43de2fe
Unknown <dominik.trusinski@gmail.com>
2018-10-08 21:48:41 +0200

    Refactor authentication service due to deletion of FB authentication option

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { flatMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 import { User } from '../models/user';
-
-import { FacebookService } from './facebook.service';
 
 import { environment } from '../../environments/environment.generated.dev';
 
@@ -14,52 +11,22 @@ import { environment } from '../../environments/environment.generated.dev';
 })
 export class AuthenticationService {
   constructor(
-    private http: HttpClient,
-    private facebookService: FacebookService
+    private http: HttpClient
   ) { }
 
   signIn(user: User): Observable<any> {
-    return this.makeAuthenticationReq(AuthMethod.IN_APP, user);
+    return this.makeAuthenticationReq(user);
   }
 
-  signInWithFb(): Observable<any> {
-    if(this.facebookService.isUserSignedIn) {
-      return this.facebookService.getUserEmail()
-        .pipe(
-          flatMap(resp => of({ email: resp['email'] , accessToken: FB.getAuthResponse().accessToken })),
-          flatMap(resp => this.makeAuthenticationReq(AuthMethod.FACEBOOK, resp)));
-    }
+  private makeAuthenticationReq(userData: Object): Observable<Object> {
+    const payload = {
+      auth_method: 'in_app',
+      user: {
+        email: userData['email'],
+        password: userData['password']
+      }
+    };
 
-    return this.facebookService.signInUser()
-      .pipe(
-        flatMap(() => this.facebookService.getUserEmail()),
-        flatMap(resp => of({ email: resp.authResponse, accessToken: FB.getAuthResponse().accessToken })),
-        flatMap(resp => this.makeAuthenticationReq(AuthMethod.FACEBOOK, resp))
-      );
-  }
-
-  private makeAuthenticationReq(authMethod: AuthMethod, userData: Object): Observable<Object> {
-    const payload = this.calculatePayload(authMethod, userData);
     return this.http.post(environment.api.signInURL, payload);
   }
-
-  private calculatePayload(authMethod: AuthMethod, userData: Object): Object {
-    const payload = {
-      auth_method: authMethod,
-      user: {}
-    }
-
-    if (authMethod === AuthMethod.IN_APP) {
-      payload.user = { email: userData['email'], password: userData['password'] };
-    } else {
-      payload.user = { email: userData['email'], access_token: userData['accessToken'] };
-    }
-
-    return payload;
-  }
-}
-
-enum AuthMethod {
-  IN_APP = 'in_app',
-  FACEBOOK = 'facebook'
 }
