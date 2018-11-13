cea2e62156d6df8dee049a1eada30cf8f91ade9f
Dominik Trusinski <dominik.trusinski@gmail.com>
2018-11-13 22:21:13 +0100

    Redirect http to https

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express'),
       http = require('http'),
       path = require('path'),
       app = express(),
-      port = process.env.PORT || '8080';
+      port = process.env.PORT || '80',
+      forceSsl = (req, res, next) => {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+          return res.redirect(['https://', req.get('Host'), req.url].join(''));
+        }
+        return next();
+      };
 
 app.use(express.static(path.join(__dirname, 'dist/vinci')));
+app.use(forceSsl);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/vinci/index.generated.html'));
