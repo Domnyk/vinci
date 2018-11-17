0381d63d79a9eea992ca6fbef7359f8d4e9a4ee7
Unknown <dominik.trusinski@gmail.com>
2018-11-18 00:03:20 +0100

    Fix typos

--- src/app/models/dto.ts
+++ src/app/models/dto.ts
@@ -1,7 +1,7 @@
 export interface DTO {
   /**
-   * This method should be called when Typescript show is meant to be send to API. Here all differences in show structure should be
-   * changed in favour of API so it can be safely send.
+   * This method should be called when Typescript object is meant to be send to API.
+   * Here all differences in object structure should be changed in favour of API so it can be safely send.
    *
    * @returns {Object} Object with proper structure
    */
