790a6917f8b184393e1c10e5a879b0619bff2821
Dominik Trusinski <dominik.trusinski@gmail.com>
2018-12-01 18:49:08 +0100

    Delete mock responsible for authorization of owner dashboard access. Use real authorization

--- src/app/guards/admin-authorization.guard.ts
+++ src/app/guards/admin-authorization.guard.ts
@@ -1,9 +1,8 @@
 import { Observable, of } from 'rxjs';
 import { CanActivate, Router } from '@angular/router';
-import { AuthorizationService } from '../services/authorization.service';
 import { Store } from '@ngxs/store';
 import { Injectable } from '@angular/core';
-import { flatMap, map } from 'rxjs/operators';
+import { flatMap } from 'rxjs/operators';
 import { UserType } from '../models/current-user';
 
 @Injectable({
@@ -12,29 +11,11 @@ import { UserType } from '../models/current-user';
 export class AdminAuthorizationGuard implements CanActivate {
 
   constructor(
-    private authorizationService: AuthorizationService,
     private store: Store,
     private router: Router
   ) { }
 
   canActivate(): Observable<boolean> {
-    // TODO: Uncomment later. For now it is convenience mock
-
-    // console.warn('AdminAuthorizationGuard.canActive is mocked and returns always true');
-    // return of(true);
-    // const adminStatusHandler: (value: boolean) => boolean = isAdmin => {
-    //   if (isAdmin) {
-    //     return true;
-    //   } else {
-    //     this.router.navigate(['/']);
-    //     return false;
-    //   }
-    // };
-
-    // return this.authorizationService.isAdmin(this.store).pipe(
-    //   map(adminStatusHandler)
-    // );
-
     return this.store.select(state => state.currentUser.type)
       .pipe(
         flatMap((userType: UserType) => of(userType === UserType.ComplexesOwner))
