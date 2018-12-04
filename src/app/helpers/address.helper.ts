0aca056098f57d38eba4dfce199b0c6eeed008c8
Unknown <dominik.trusinski@gmail.com>
2018-12-04 20:14:29 +0100

    Print address of sport object

--- src/app/helpers/address.helper.ts
+++ src/app/helpers/address.helper.ts
@@ -4,14 +4,12 @@ import { Observable, of } from 'rxjs';
 import { flatMap } from 'rxjs/operators';
 
 export class AddressHelper {
-  static printAddress(object$: Observable<SportObject>): Observable<string> {
+  static printAddress(object: SportObject): string {
     const getAddressAsString = (address: BuildingAddressLiteral): string => {
       const { street, buildingNumber, city, postalCode } = address;
       return `${street} ${buildingNumber}, ${postalCode} ${city}`;
     };
 
-    return object$.pipe(
-      flatMap((object: SportObject) => of(getAddressAsString(object.address)))
-    );
+    return getAddressAsString(object.address);
   }
 }
