10b003d0522c606f319428bf258594e17af3254a
Dominik Trusinski <dominik.trusinski@gmail.com>
2018-12-30 21:05:49 +0100

    Add autocomplete attribute to inputs

---
+++ src/app/components/common/form-field/autocomplete-type.ts
@@ -0,0 +1,7 @@
+export enum AutocompleteType {
+  OFF = 'off',
+  ON = 'on',
+  EMAIL = 'email',
+  NEW_PASSWORD = 'new-password',
+  CURRENT_PASSWORD = 'current-password'
+}
--- src/app/components/common/form-field/form-field.component.ts
+++ src/app/components/common/form-field/form-field.component.ts
@@ -1,7 +1,8 @@
-import { Component, HostBinding, Input, OnChanges, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
+import { Component, HostBinding, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { FormHelper } from '../../../helpers/form.helper';
 import { SelectParams } from './select-params';
+import { AutocompleteType } from './autocomplete-type';
 
 @Component({
   selector: 'app-form-field',
@@ -18,6 +19,7 @@ export class FormFieldComponent implements OnInit, OnChanges {
   @Input() rootClass?: string;
   @Input() selectParams?: SelectParams;
   @Input() readonly?: boolean;
+  @Input() autocomplete?: string;
 
   /*
     This binding solves problem of .row and .col relationship
@@ -31,6 +33,26 @@ export class FormFieldComponent implements OnInit, OnChanges {
   _class: string = null;
   _isSelect: boolean = null;
   _isReadonly: boolean = null;
+  _autocomplete: string = null;
+
+  // TODO: 'new-password' should return AutocompleteType.NEW_PASSWORD but this would require this.type to be 'new-password'
+  // TODO: this.type is also used in input as attr.type
+  private static getAutocompleteType(inputType: string): AutocompleteType {
+    switch (inputType) {
+      case 'email':
+        return AutocompleteType.EMAIL;
+      case 'password':
+        return AutocompleteType.CURRENT_PASSWORD;
+      case 'new-password':
+        return AutocompleteType.CURRENT_PASSWORD;
+      case undefined:
+        return AutocompleteType.ON;
+      case null:
+        return AutocompleteType.ON;
+      default:
+        return AutocompleteType.ON;
+    }
+  }
 
   constructor() { }
 
@@ -44,6 +66,12 @@ export class FormFieldComponent implements OnInit, OnChanges {
 
     this._isSelect = this.type === 'select';
     this._isReadonly = !!this.readonly ? this.readonly : false;
+
+    if (!!this.autocomplete) {
+      this._autocomplete = this.autocomplete;
+    } else {
+      this._autocomplete = FormFieldComponent.getAutocompleteType(this.type);
+    }
   }
 
   ngOnChanges({ readonly }: SimpleChanges) {
