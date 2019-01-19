f56759d0f23600e03a916457adc4b3585ba45437
Dominik Trusinski <dominik.trusinski@gmail.com>
2019-01-19 19:33:30 +0100

    CSRF sucks...

--- src/app/services/csrf.interceptor.ts
+++ src/app/services/csrf.interceptor.ts
@@ -10,9 +10,14 @@ export class CsrfInterceptor implements HttpInterceptor {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const headerName = 'x-csrf-token',
+    const headerName = 'X-CSRF-Token',
       token = this.tokenExtractor.getToken() as string;
+
+    console.log('token !== null: ', token !== null);
+    console.log('!req.headers.has(headerName): ', !req.headers.has(headerName));
+
     if (token !== null && !req.headers.has(headerName)) {
+      console.log('Adding csrf header');
       req = req.clone({ headers: req.headers.set(headerName, token) });
     }
     return next.handle(req);
