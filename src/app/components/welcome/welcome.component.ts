c7188808e022a0efef87b3fc2f5ba15656725a43
Dominik Trusinski <dominik.trusinski@gmail.com>
2019-01-23 19:16:33 +0100

    Welcome page changes

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment.generated.dev';
 
 @Component({
   selector: 'app-welcome',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+  backendSignInAddress = environment.api.urls.signIn();
 
   constructor() { }
 
